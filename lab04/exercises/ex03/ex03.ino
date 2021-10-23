681e4cff46c13821ceefc294e0b817a70398769f
Karol-Waliszewski <karolwaliszewski@gmail.com>
2021-10-23 22:18:47 +0200

    Added another decimal place in ex03

--- lab04/exercises/ex03/ex03.ino
+++ lab04/exercises/ex03/ex03.ino
@@ -4,7 +4,7 @@ LiquidCrystal lcd(12, 11, 7, 10, 9, 8);
 
 // #include <LiquidCrystal_I2C.h>
 // LiquidCrystal_I2C lcd(0x27, 16, 2);
-int MAX_VALUE = 1023;
+float MAX_VALUE = 1023;
 int prevValue;
 
 void setup()
@@ -21,8 +21,8 @@ void loop()
       lcd.setCursor(0, 0);
       lcd.print(value);
       lcd.setCursor(0, 1);
-      lcd.print((float)(5.0 * value/MAX_VALUE), 2);
-      lcd.setCursor(4, 1);
+      lcd.print((float)(5.00 * value/MAX_VALUE), 3);
+      lcd.setCursor(5, 1);
       lcd.print('V');
     }
   	prevValue = value;
